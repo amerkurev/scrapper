cbcee138fe6f24b98b6367d5f27764a8f751fbfc
amerkurev <apps.merkurev@gmail.com>
2023-03-11 12:48:07 +0300

    create request URL

--- static/scrape-it.js
+++ static/scrape-it.js
@@ -1,24 +1,26 @@
 "use strict";
 !function() {
-    function formatParams(params) {
+    function formatParams() {
+        var url = document.getElementById("url").value;
+        if (!url) return "";
+        var params = {
+          url: url
+        };
         return "?" + Object
             .keys(params)
             .map((key) => {
                 return key+"="+encodeURIComponent(params[key])
             })
-            .join("&")
+            .join("&");
     };
 
     var scrapeIt = document.getElementById("scrape-it");
     scrapeIt.addEventListener("click", (e) => {
         e.preventDefault();
-        var url = document.getElementById("url").value;
-        if (!url) {
-            return
-        }
-        var params = {url: url};
+        var params = formatParams();
+        if (params === "") return;
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "/parse" + formatParams(params), true);
+        xhr.open("GET", "/parse" + params, true);
         xhr.send();
         xhr.onreadystatechange = (e) => {
             if (xhr.readyState == 4 && xhr.status == 200) {
@@ -29,5 +31,23 @@
         };
         scrapeIt.innerHTML = "Please wait…";
         scrapeIt.setAttribute("aria-busy", "true");
+    });
+
+    // watch for changes in the query params
+    var url = document.getElementById("url");
+    url.addEventListener("input", (e) => {
+        var params = formatParams();
+        var snippet = document.getElementById("snippet");
+        var snippetLink = document.getElementById("snippetLink");
+        var snippetLabel = document.getElementById("snippetLabel");
+        if (params === "") {
+          snippet.style.display = "none";
+        } else {
+          var hostname = window.location.protocol + "//" + window.location.host;
+          snippetLink.innerHTML = hostname + params;
+          snippetLink.setAttribute("href", "/parse" + params);
+          snippetLabel.innerHTML = "Request URL:"
+          snippet.style.display = "block";
+        }
     });
 }();
