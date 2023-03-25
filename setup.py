9100bfe32e476760254b7abc2da40d7937c76953
amerkurev <apps.merkurev@gmail.com>
2023-03-25 18:45:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
         'Jinja2==3.1.2',
         'beautifulsoup4==4.11.2',
         'waitress==2.1.2',
+        'tldextract==3.4.0',
         'validators==0.20.0',
     ],
     # version='0.0.0',
