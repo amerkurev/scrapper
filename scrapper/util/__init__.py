e44a7322bd06668acef05047637cfd3d551d70bc
amerkurev <apps.merkurev@gmail.com>
2023-04-10 12:26:19 +0300

    delete unnecessary comment

--- scrapper/util/__init__.py
+++ scrapper/util/__init__.py
@@ -18,7 +18,7 @@ def levenshtein_similarity(str1, str2):
     for i in range(1, len(str1) + 1):
         for j in range(1, len(str2) + 1):
             if str1[i - 1] == str2[j - 1]:
-                d[i][j] = d[i - 1][j - 1]  # символы совпадают, расстояние не увеличивается
+                d[i][j] = d[i - 1][j - 1]
             else:
                 d[i][j] = min(d[i - 1][j], d[i][j - 1], d[i - 1][j - 1]) + 1
 
